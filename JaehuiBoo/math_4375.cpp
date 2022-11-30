01901c94002b179d4e9dd0888e5b9872c54097a3
JaeHuiBoo <yeppyboo@naver.com>
2022-11-29 23:37:28 -0500

    math_4375_O

---
+++ JaehuiBoo/math_4375.cpp
@@ -0,0 +1,26 @@
+#include <iostream>  //전처리 지시자
+#include <string>
+
+using namespace std; 
+
+int main(){
+    // 2와 5로 나눠 쩔어지지 않는 정수 n
+    // 1로만 이뤄진 n의 배수의 가장 작은 수의 자리수 
+    int in;
+    while(cin >> in)
+    {
+        int remain = 1;
+        int num = 1;
+        while(true){
+            if(remain%in==0)
+                break;
+            else{
+                remain = (remain%in)*10+1;
+                num++;
+            }
+        }
+        cout << num << endl;
+    }
+    return 0;
+
+}
